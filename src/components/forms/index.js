f29b5a2a9f084929c03e4fe4a716189ffdca3b0d
JoeSieverding <joesieverding@hotmail.com>
2025-04-24 08:59:32 -0400

    new file:   src/components/forms/ScaCreateForm.d.ts 	new file:   src/components/forms/ScaCreateForm.jsx 	new file:   src/components/forms/ScaUpdateForm.d.ts 	new file:   src/components/forms/ScaUpdateForm.jsx 	new file:   src/components/forms/index.js 	modified:   src/components/home/ScaDetail.tsx 	modified:   src/components/home/ScaList.tsx 	new file:   src/components/home/ScaMilestoneList.tsx 	new file:   src/components/trunk/ScaDetail.tsx 	new file:   src/components/trunk/ScaList.tsx

---
+++ src/components/forms/index.js
@@ -0,0 +1,2 @@
+export { default as ScaCreateForm } from "./ScaCreateForm";
+export { default as ScaUpdateForm } from "./ScaUpdateForm";
---
+++ src/components/home/ScaMilestoneList.tsx
@@ -0,0 +1,10 @@
+function ScaMilestoneList()
+{
+    return (
+        <div>
+            <h1>ScaMilestoneList</h1>
+        </div>
+    )
+}
+
+export default ScaMilestoneList;
