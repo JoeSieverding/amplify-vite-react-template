727dfbf9798baf3f1f06f7b6443fbb4b2249338d
JoeSieverding <joesieverding@hotmail.com>
2025-04-28 14:48:42 -0400

    modified:   src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
-//import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
@@ -10,17 +9,38 @@ import TopNav from "./components/Common/TopNav";
 import "@cloudscape-design/global-styles/index.css"
 import { ThemeProvider } from '@aws-amplify/ui-react';
 import { BrowserRouter } from "react-router-dom";
+
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  
   <React.StrictMode>
-    <Authenticator>
-    <BrowserRouter>
-      <ThemeProvider>
-        <TopNav />
-        <App />
-      </ThemeProvider>
+    <Authenticator
+      signUpAttributes={['email']}
+      formFields={{
+        signUp: {
+          email: {
+            placeholder: "Enter your email",
+            label: "Email",
+            isRequired: true
+          }
+        }
+      }}
+      services={{
+        validateCustomSignUp: async (formData) => {
+          const email = formData.email;
+          if (!email.endsWith('@amazon.com')) {
+            return {
+              email: 'Only @amazon.com email addresses are allowed'
+            };
+          }
+        }
+      }}
+    >
+      <BrowserRouter>
+        <ThemeProvider>
+          <TopNav />
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </Authenticator>
   </React.StrictMode>
